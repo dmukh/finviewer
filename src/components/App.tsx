2b442147c650edb3eb42e55230c38cfc3f924574
Deep Mukherji <deep.mukherji@utexas.edu>
2019-12-14 13:40:43 -0600

    Added editable table

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react'
-import Line from './charts/Line'
-import './App.css'
+import Line from './viz/Line'
+
+import EditableTable from './viz/EditableTable'
+import './viz/EditableTable.css'
 
 export default class App extends React.Component {
   public render() {
     return (
       <div className="App">
         <Line />
+        <EditableTable />
       </div>
     )
   }
